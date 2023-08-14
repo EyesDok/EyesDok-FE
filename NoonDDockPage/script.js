55790f5437f2441df0912ef97b866f5071f36a20
Na._.kyung <leena3823@gmail.com>
2023-08-14 13:52:00 +0900

    나의 눈똑 페이지 연결

--- NoonDDockPage/script.js
+++ NoonDDockPage/script.js
@@ -1,3 +1,11 @@
+const writeButton = document.getElementById('write-button');
+
 function toggleHeart(button) {
     button.classList.toggle('active'); 
   }
+
+
+writeButton.addEventListener('click', function () {
+  // 다른 페이지로 이동
+  window.location.href = "/WritePage/Write.html";
+});
